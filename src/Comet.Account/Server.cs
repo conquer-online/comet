4d89166bfea36238ccdfd6890bf4f1f2cc5a7cab
Spirited <git@spirited.io>
2018-09-16 19:57:16 -0700

    Added packet processing channel

--- src/Comet.Account/Server.cs
+++ src/Comet.Account/Server.cs
@@ -3,6 +3,7 @@ namespace Comet.Account
     using System;
     using System.Net.Sockets;
     using Comet.Account.States;
+    using Comet.Network.Packets;
     using Comet.Network.Security;
     using Comet.Network.Sockets;
 
@@ -13,6 +14,19 @@ namespace Comet.Account
     /// </summary>
     internal sealed class Server : TcpServerListener
     {
+        // Fields and Properties
+        private readonly PacketProcessor Processor;
+
+        /// <summary>
+        /// Instantiates a new instance of <see cref="Server"/> by initializing the 
+        /// <see cref="PacketProcessor"/> for processing packets from the players using 
+        /// channels and worker threads. Initializes the TCP server listener.
+        /// </summary>
+        public Server() : base()
+        {
+            this.Processor = new PacketProcessor(this.Process);
+        }
+
         /// <summary>
         /// Invoked by the server listener's Accepting method to create a new server actor
         /// around the accepted client socket. Gives the server an opportunity to initialize
@@ -24,6 +38,31 @@ namespace Comet.Account
         protected override TcpServerActor Accepted(Socket socket, Memory<byte> buffer)
         {
             return new Client(socket, buffer);
+        }
+
+        /// <summary>
+        /// Invoked by the server listener's Receiving method to process a completed packet
+        /// from the actor's socket pipe. At this point, the packet has been assembled and
+        /// split off from the rest of the buffer.
+        /// </summary>
+        /// <param name="actor">Server actor that represents the remote client</param>
+        /// <param name="packet">Packet bytes to be processed</param>
+        protected override void Received(TcpServerActor actor, ReadOnlySpan<byte> packet)
+        {
+            this.Processor.Queue(actor, packet.ToArray());
+        }
+
+        /// <summary>
+        /// Invoked by one of the server's packet processor worker threads to process a
+        /// single packet of work. Allows the server to process packets as individual 
+        /// messages on a single channel.
+        /// </summary>
+        /// <param name="actor">Actor requesting packet processing</param>
+        /// <param name="packet">An individual data packet to be processed</param>
+        private void Process(TcpServerActor actor, byte[] packet) 
+        {
+            Console.WriteLine("Processing {0} bytes", packet.Length);
+            Console.WriteLine(PacketDump.Hex(packet));
         }
     }
 }
---
+++ src/Comet.Network/Packets/PacketProcessor.cs
@@ -0,0 +1,105 @@
+namespace Comet.Network.Packets
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Channels;
+    using System.Threading.Tasks;
+    using Comet.Network.Sockets;
+
+    /// <summary>
+    /// Packet processor for handling packets in worker threads using unbounded channels.
+    /// Allows for multiple writers, such as each remote client's accepted socket receive
+    /// loop, to write to the channel. Multiple readers then read single messages from
+    /// the channel for processing.
+    /// </summary>
+    public class PacketProcessor
+    {
+        // Fields and Properties
+        protected readonly Channel<Message> Messages;
+        protected readonly Action<TcpServerActor, byte[]> Process;
+        protected readonly List<Task> Tasks;
+        protected CancellationToken CancelReads;
+        protected CancellationToken CancelWrites;
+
+        /// <summary>
+        /// Instantiates a new instance of <see cref="PacketProcessor"/> using a default
+        /// amount of worker threads to initialize. Creates threads and starts threads
+        /// immediately to read data from the unbounded channel.
+        /// </summary>
+        /// <param name="process">Processing action for channel messages</param>
+        /// <param name="cancelReads">Token for cancelling reads</param>
+        /// <param name="cancelWrites">Token for cancelling writes</param>
+        /// <param name="count">Number of threads to be created</param>
+        public PacketProcessor(
+            Action<TcpServerActor, byte[]> process, 
+            CancellationToken cancelReads = default(CancellationToken),
+            CancellationToken cancelWrites = default(CancellationToken),
+            int count = 0)
+        {
+            // Initialize the channel
+            this.CancelReads = cancelReads;
+            this.CancelWrites = cancelWrites;
+            this.Messages = Channel.CreateUnbounded<Message>();
+            this.Process = process;
+            this.Tasks = new List<Task>();
+
+            // Create threads for reading from the channel
+            count = count == 0 ? Environment.ProcessorCount : count;
+            for (int i = 0; i < count; i++)
+                this.Tasks.Add(Task.Run(this.Dequeue));
+        }
+
+        /// <summary>
+        /// Queues work by writing to the messages channel. Work is queued by a connected
+        /// client, and dequeued by the server's packet processing worker threads. Each
+        /// work item contains a single packet to be processed.
+        /// </summary>
+        /// <param name="actor">Actor requesting packet processing</param>
+        /// <param name="packet">Packet bytes to be processed</param>
+        public void Queue(TcpServerActor actor, byte[] packet)
+        {
+            if (!this.CancelWrites.IsCancellationRequested)
+                this.Messages.Writer.TryWrite(new Message {
+                    Actor = actor,
+                    Packet = packet
+                });
+        }
+
+        /// <summary>
+        /// Dequeues work in a loop. For as long as the thread is running and work is 
+        /// available, work will be dequeued and processed. After dequeuing a message,
+        /// the packet processor's <see cref="Process"/> action will be called. 
+        /// </summary>
+        private async Task Dequeue()
+        {
+            while (!this.CancelReads.IsCancellationRequested)
+            {
+                var msg = await this.Messages.Reader.ReadAsync(this.CancelReads);
+                this.Process(msg.Actor, msg.Packet);
+            }
+        }
+
+        /// <summary>
+        /// Shuts down reads and then writes for the packet processor, allowing for the
+        /// processing channel to be completely processed before shutting down threads.
+        /// </summary>
+        public void Shutdown()
+        {
+            this.CancelWrites = new CancellationToken(true);
+            this.Messages.Reader.Completion.Wait();
+            this.CancelReads = new CancellationToken(true);
+        }
+        
+        /// <summary>
+        /// Defines a message for the <see cref="PacketProcessor"/>'s unbounded channel
+        /// for queuing packets and actors requesting work. Each message defines a single
+        /// unit of work - a single packet for processing. 
+        /// </summary>
+        protected class Message
+        {
+            public TcpServerActor Actor;
+            public byte[] Packet;
+        }
+    }
+}
