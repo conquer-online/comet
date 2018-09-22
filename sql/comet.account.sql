2216a61bb5739c8b3b390eb69f0d904841db83d8
Spirited <git@spirited.io>
2018-09-22 14:36:02 -0700

    Encoding fix for MySQL CI

--- sql/comet.account.sql
+++ sql/comet.account.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `comet.account` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+CREATE DATABASE  IF NOT EXISTS `comet.account` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
 USE `comet.account`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
@@ -133,6 +133,10 @@ LOCK TABLES `logins` WRITE;
 UNLOCK TABLES;
 
 --
+-- Dumping events for database 'comet.account'
+--
+
+--
 -- Dumping routines for database 'comet.account'
 --
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -145,4 +149,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-09-22 13:33:06
+-- Dump completed on 2018-09-22 14:35:27
