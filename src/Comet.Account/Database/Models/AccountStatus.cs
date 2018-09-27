a534f91fca5d4b5390e511ab5a66c5b3d589d07d
Spirited <git@spirited.io>
2018-09-26 19:51:29 -0700

    Cleaning up usings

--- src/Comet.Account/Database/Models/AccountStatus.cs
+++ src/Comet.Account/Database/Models/AccountStatus.cs
@@ -1,8 +1,6 @@
 ﻿namespace Comet.Account.Database.Models
 {
-    using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
 
     /// <summary>
