54682024748fdb993ef32570aeca90c7044e5621
Spirited <git@spirited.io>
2020-12-11 23:19:17 -0800

    Add client pool and duplicated login check

--- src/Comet.Game/Packets/MsgConnect.cs
+++ src/Comet.Game/Packets/MsgConnect.cs
@@ -63,6 +63,15 @@ namespace Comet.Game.Packets
                 return;
             }
 
+            // If another client has already connected using this account, disconnect
+            // that other client and permit this one to take its place.
+            if (Kernel.Clients.TryRemove(auth.AccountID, out Client observer))
+            {
+                observer.Socket.Disconnect(false);
+            }
+
+            Kernel.Clients.TryAdd(auth.AccountID, client);
+
             // Generate new keys and check for an existing character
             var character = await CharactersRepository.FindAsync(auth.AccountID);
             if (character == null)
