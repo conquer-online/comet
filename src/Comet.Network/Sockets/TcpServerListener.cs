f91871cbf19b27616f7f12b00d821c383d6eb8b4
Spirited <git@spirited.io>
2021-06-04 13:45:19 -0700

    Fix potential denial of service exploit

--- src/Comet.Network/Sockets/TcpServerListener.cs
+++ src/Comet.Network/Sockets/TcpServerListener.cs
@@ -191,6 +191,7 @@ namespace Comet.Network.Sockets
             while (consumed + 2 < examined)
             {
                 var length = BitConverter.ToUInt16(buffer.Slice(consumed, 2));
+                if (length == 0) return false;
                 var expected = consumed + length;
                 if (length > buffer.Length) return false;
                 if (expected > examined) break;
