bbbe6906c7896343f0568388854d90abd08cad70
Spirited <git@spirited.io>
2022-10-18 11:37:02 -0700

    Update projects to .NET 6 and C# 10

--- src/Comet.Game/Server.cs
+++ src/Comet.Game/Server.cs
@@ -19,6 +19,7 @@ namespace Comet.Game
     {
         // Fields and Properties
         private readonly PacketProcessor<Client> Processor;
+        private readonly Task ProcessorTask;
 
         /// <summary>
         /// Instantiates a new instance of <see cref="Server"/> by initializing the 
@@ -29,7 +30,7 @@ namespace Comet.Game
         public Server(ServerConfiguration config) : base(maxConn: config.GameNetwork.MaxConn)
         {
             this.Processor = new PacketProcessor<Client>(this.ProcessAsync);
-            this.Processor.StartAsync(CancellationToken.None).ConfigureAwait(false);
+            this.ProcessorTask = this.Processor.StartAsync(CancellationToken.None);
         }
 
         /// <summary>
